33978f538595342e6a00b31cda9287e1bd31fbf6
Juan G. Jordán <juangui@gmail.com>
2017-05-31 09:18:44 +0200

    Update Dashboard_4.js
    The travelService.getDeparture function already calls user.email, thus returning undefined for the userID.

--- src/Dashboard_4.js
+++ src/Dashboard_4.js
@@ -6,7 +6,7 @@
 
             var loadFlight = function( user )
                 {
-                    return travelService.getDeparture( user.email );               // Request #1
+                    return travelService.getDeparture( user );               // Request #1
                 },
                 loadFlightWeather = function ( departure )
                 {
